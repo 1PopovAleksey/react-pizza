1cb759a57988663524641fe03beb2914d0b9f10f
Aleksey Popov <1PopovAleksey@gmail.com>
2022-06-09 23:51:46 +0700

    Fix the class and remove the empty space.

--- src/App.js
+++ src/App.js
@@ -9,32 +9,24 @@ import "./scss/app.scss";
 
 function App() {
   return (
-    <div class="wrapper">
-    <Header/>
-    <div class="content">
-      <div class="container">
-        <div class="content__top">
-          <Categories/>
-          <Sort />
-        </div>
-        <h2 class="content__title">Все пиццы</h2>
-        <div class="content__items">
-          <PizzaBlock />
-          <PizzaBlock />
-          <PizzaBlock />
-          <PizzaBlock />
-          <PizzaBlock />
-          <PizzaBlock />
-          <PizzaBlock />
-          <PizzaBlock />
-          <PizzaBlock />
-          <PizzaBlock />
-          <PizzaBlock />
-          <PizzaBlock />
+    <div className="wrapper">
+      <Header/>
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories/>
+            <Sort/>
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title='Мексиканская' price={599}/>
+            <PizzaBlock title='Вкусная' price={599}/>
+            <PizzaBlock title='Не вкусная' price={599}/>
+            <PizzaBlock title='Обычная' price={599}/>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 }
 
